581c077b473c1027a457584882b06b5797bc7236
lma0825 <lma95082528@gmail.com>
2013-07-02 18:51:40 +0900

    echoserver2.js 파일명 echoserver_completion.js로 수정

--- echoserver2.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var app = express();
-
-app.get('/list/:input', function(req, res){
-    res.send('Input Value : ' + req.params.input);
-});
-app.listen(8000);
-console.log('Start echoserver');
