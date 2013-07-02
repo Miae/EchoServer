33f2bd186fdb11ca2a6a26936c84c0a645421da5
lma0825 <lma95082528@gmail.com>
2013-07-02 20:38:13 +0900

    echoserver_completion.js 재커밋

--- echoserver_completion.js
+++ echoserver_completion.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/list/:input', function(req, res){
-    res.send('Input Value : ' + req.params.input);
+    res.send('Input Value!!!~ : ' + req.params.input);
 });
 app.listen(8000);
 console.log('Start echoserver');
